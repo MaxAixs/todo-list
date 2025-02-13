fd42d1c9d6808b8fb884fbb6c831589370f9205d
Aix <Aix60689100@gmail.com>
2025-02-13 18:35:16 +0200

    add DeadlineWorker for periodic Deadline checks and notifications

---
+++ pkg/notifyService/worker/deadlineWorker.go
@@ -0,0 +1,46 @@
+package worker
+
+import (
+	"context"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"time"
+	notifyServices "todo-list/pkg/notifyService/service"
+	"todo-list/todo/repository"
+)
+
+type DeadlineWorker struct {
+	repo    *repository.Repository
+	service notifyServices.NotifyService
+}
+
+func NewDeadlineWorker(repo *repository.Repository, service notifyServices.NotifyService) *DeadlineWorker {
+	return &DeadlineWorker{repo: repo, service: service}
+}
+
+func (c *DeadlineWorker) Start(ctx context.Context) error {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			logrus.Println("worker shutting down")
+			return ctx.Err()
+
+		case <-ticker.C:
+			usersWithDeadlines, err := c.repo.GetDeadlineItems()
+			if err != nil {
+				return fmt.Errorf("error fetching users with expiring deadlines: %v", err)
+			}
+
+			if len(usersWithDeadlines) > 0 {
+				logrus.Printf("Found %d users with expiring deadlines", len(usersWithDeadlines))
+			}
+
+			if err := c.service.PushToNotifyService(usersWithDeadlines); err != nil {
+				return fmt.Errorf("error sending deadline to notification service: %v", err)
+			}
+		}
+	}
+}
