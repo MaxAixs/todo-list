7fffa5c1f32b3ae8b8f7d8d6e1d1b9ef9a54dd84
Aix <Aix60689100@gmail.com>
2025-02-23 21:23:50 +0200

    implement AnalyticWorker to send completed tasks data to analytics service

---
+++ pkg/analyticsService/worker/analyticWorker.go
@@ -0,0 +1,49 @@
+package worker
+
+import (
+	"context"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"time"
+	"todo-list/pkg/analyticsService/service"
+	"todo-list/todo/repository"
+)
+
+type AnalyticWorker struct {
+	repo       *repository.Repository
+	gRPCClient service.AnalyticsGrpcService
+}
+
+func NewAnalyticWorker(repo *repository.Repository, gRPCClient service.AnalyticsGrpcService) *AnalyticWorker {
+	return &AnalyticWorker{repo: repo, gRPCClient: gRPCClient}
+}
+
+func (a *AnalyticWorker) Start(ctx context.Context) error {
+	ticker := time.NewTicker(2 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			logrus.Print("analyticWorker shutting down")
+			return ctx.Err()
+
+		case <-ticker.C:
+			usersWithCompletedTasks, err := a.repo.GetUsersWithDoneItem()
+			if err != nil {
+				return fmt.Errorf("error fetching users with done item: %w", err)
+			}
+
+			if len(usersWithCompletedTasks) > 0 {
+				logrus.Printf("analyticWorker got users %v with completed tasks", len(usersWithCompletedTasks))
+			}
+
+			if err := a.gRPCClient.SendDoneItems(usersWithCompletedTasks); err != nil {
+				return fmt.Errorf("error sending data to analytics service: %w", err)
+			}
+
+			logrus.Info("successfully sent data to analytics service")
+		}
+	}
+
+}
