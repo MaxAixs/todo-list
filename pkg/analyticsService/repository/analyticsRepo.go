75382006424547db1c2e11c7d70f6c1485bda86c
Aix <Aix60689100@gmail.com>
2025-02-24 19:11:57 +0200

    change sql query

--- pkg/analyticsService/repository/analyticsRepo.go
+++ pkg/analyticsService/repository/analyticsRepo.go
@@ -18,21 +18,24 @@ func (a *AnalyticRepo) GetUsersWithDoneItem() ([]analyticsService.TaskDoneItem, 
 	var items []analyticsService.TaskDoneItem
 
 	query := `
-		SELECT ti.user_id, u.email, ti.id, ti.created_at
-		FROM todo_items ti
-		JOIN users u ON ti.user_id = u.id
-		WHERE ti.done = true AND ti.sent_analys = false;
+		UPDATE todo_items ti
+		SET sent_analys = true
+		FROM users u
+		WHERE ti.user_id = u.id
+		AND ti.done = true
+		AND ti.sent_analys = false
+		RETURNING u.email, ti.user_id, ti.id AS item_id, ti.created_at;
 	`
 
 	rows, err := a.db.Query(query)
 	if err != nil {
-		return nil, fmt.Errorf("error getting users with done items: %v", err)
+		return nil, fmt.Errorf("error updating and getting users with done items: %v", err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
 		var item analyticsService.TaskDoneItem
-		if err := rows.Scan(&item.UserID, &item.Email, &item.ItemID, &item.CreatedAt); err != nil {
+		if err := rows.Scan(&item.Email, &item.UserID, &item.ItemID, &item.CreatedAt); err != nil {
 			fmt.Printf("error scan users with done items: %v", err)
 		}
 		items = append(items, item)
